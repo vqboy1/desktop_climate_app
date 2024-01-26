71f44020573091913868cb8f85479049c6acb4f5
vqboy1 <152992245+vqboy1@users.noreply.github.com>
2024-01-26 14:25:22 +0300

    Add files via upload
    added folder with 4 files (1st commit)

---
+++ climate_app/api.py
@@ -0,0 +1,2 @@
+def get_weather():
+    return {"temp": -2, "humidity": 20, "state": "Облачно"}
---
+++ climate_app/testst.py
@@ -0,0 +1,37 @@
+import sys
+
+from PyQt6 import QtWidgets
+
+from mainWindowApp import Ui_Form
+
+from api import get_weather
+
+
+class MainApp(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent=None)
+        self.setupUi(self)
+
+        self.stackedWidget.setCurrentIndex(0)
+
+        self.btn_login.clicked.connect(self.on_login)
+
+        self.btn_get_weather.clicked.connect(self.get_weather)
+
+    def on_login(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def get_weather(self):
+        stats = get_weather()
+
+        self.label_weather.setText(f"{stats['temp']} °C\n"
+                                   f"{stats['humidity']} %\n"
+                                   f"{stats['state']}")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainApp()
+
+    window.show()
+    app.exec()
