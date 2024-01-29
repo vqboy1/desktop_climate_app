1e636201535bad153dd160f8300c8468c240c6cb
vqboy1 <152992245+vqboy1@users.noreply.github.com>
2024-01-29 11:06:57 +0300

    Update api.py returns list with 2 variables

--- climate_app/api.py
+++ climate_app/api.py
@@ -1,2 +1,19 @@
 def get_weather():
-    return {"temp": -2, "humidity": 20, "state": "Облачно"}
+    import requests
+
+    # Замените YOUR_API_KEY на ваш API ключ OpenWeatherMap
+    API_KEY = 'bd5e378503939ddaee76f12ad7a97608'
+    CITY_NAME = 'Москва'
+
+    # Формируем URL для запроса
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}&lang=ru'
+
+    # Отправляем GET запрос и получаем данные о погоде
+    response = requests.get(url)
+    data = response.json()
+
+    # Извлекаем данные о погоде
+    max_temp = data['main']['temp_max']
+    min_temp = data['main']['temp_min']
+    # Выводим данные о погоде
+    return [min_temp, max_temp]
