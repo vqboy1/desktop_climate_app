e0b42bda8187d4c83585f7156279efdc9b7b8479
vqboy1 <152992245+vqboy1@users.noreply.github.com>
2024-01-30 13:50:11 +0300

    added get_time function to api.py

--- api.py
+++ api.py
@@ -2,15 +2,6 @@ def get_weather(town):
 
     import requests
 
-#  Пример словаря данных {'coord': {'lon': 37.6156, 'lat': 55.7522}, 'weather': [{'id': 804, 'main': 'Clouds',
-    #  'description': 'пасмурно', 'icon': '04d'}],
-#      'base': 'stations', 'main': {'temp': 269.32, 'feels_like': 264.18, 'temp_min': 268.79, 'temp_max': 269.9,
-    #      'pressure': 1036, 'humidity':
-#         95, 'sea_level': 1036, 'grnd_level': 1016}, 'visibility': 10000,
-    #         'wind': {'speed': 3.98, 'deg': 241, 'gust': 10.72}, 'clouds': {'all': 99}
-#     , 'dt': 1706519011, 'sys': {'type': 2, 'id': 47754, 'country': 'RU', 'sunrise': 1706506213, 'sunset': 1706536477},
-    #     'timezone': 10800, 'id': 524901,
-#      'name': 'Москва', 'cod': 200}
 
     # Замените YOUR_API_KEY на ваш API ключ OpenWeatherMap
     API_KEY = 'bd5e378503939ddaee76f12ad7a97608'
@@ -26,3 +17,30 @@ def get_weather(town):
     # Извлекаем данные о погоде
     # Выводим данные о погоде
     return data
+
+
+def get_time(data):
+
+    import pytz
+    from datetime import datetime
+    from timezonefinder import TimezoneFinder
+
+    # a = {'coord': {'lon': 37.6156, 'lat': 55.7522}, 'weather': [{'id': 804, 'main': 'Clouds',
+    #                                                              'description': 'пасмурно', 'icon': '04d'}],
+    #      'base': 'stations', 'main': {'temp': 269.32, 'feels_like': 264.18, 'temp_min': 268.79, 'temp_max': 269.9,
+    #                                   'pressure': 1036, 'humidity':
+    #                                       95, 'sea_level': 1036, 'grnd_level': 1016}, 'visibility': 10000,
+    #      'wind': {'speed': 3.98, 'deg': 241, 'gust': 10.72}, 'clouds': {'all': 99}
+    #     , 'dt': 1706519011,
+    #      'sys': {'type': 2, 'id': 47754, 'country': 'RU', 'sunrise': 1706506213, 'sunset': 1706536477},
+    #      'timezone': 10800, 'id': 524901,
+    #      'name': 'Москва', 'cod': 200}
+
+    obj = TimezoneFinder()
+    latitude = data['coord']['lat']
+    longitude = data['coord']['lon']
+    obj = obj.timezone_at(lng=longitude, lat=latitude)
+    tz = pytz.timezone(obj)
+    datetime_town = datetime.now(tz)
+    current_time = datetime_town.strftime("%H:%M:%S")
+    return current_time
