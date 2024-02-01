6bbbc672fff92cee673a8c854354b7c2ddb62dc9
vqboy1 <belinskij_d@bk.ru>
2024-02-01 19:44:30 +0300

    условие создание логина и пароля без пробелов

--- main.py
+++ main.py
@@ -53,35 +53,39 @@ class MainApp(QtWidgets.QWidget, Ui_Form):
             self.error_label.setText("Введите логин и пароль")
         elif login in get_users():
             self.error_label.setText("Такой логин уже существует / неправильный пароль")
+        elif ' ' in login or ' ' in password:
+            self.error_label.setText("Логин и пароль не должны иметь пробелы")
         else:
             reg(login, password)
 
     def on_get_weather_press(self):
         town = self.lineEdit_town.text()
-        try:
-            stats = get_weather(town)
-            day_time = get_time(stats)
-            wind = stats['wind']
-            clouds = stats['weather'][0]['description']
-            prime = stats['main']
-            pressure = prime['pressure']
-            max_temp = prime['temp_max']
-            min_temp = prime['temp_min']
-            humidity = prime['humidity']
-            self.label_weather.setText(f"Город {stats['name']} \n"
-                                       f"Влажность: {humidity - 10}% \n"
+        if len(town) == 0:
+            self.label_weather.setText('Введите город')
+        else:
+            try:
+                stats = get_weather(town)
+                day_time = get_time(stats)
+                wind = stats['wind']
+                clouds = stats['weather'][0]['description']
+                prime = stats['main']
+                max_temp = prime['temp_max']
+                min_temp = prime['temp_min']
+                self.label_weather.setText(f"Город {stats['name']} \n"
+                                       f"Влажность: {prime['humidity'] - 10}% \n"
                                        f"Макс. темп: {round(min_temp - 273)}°C \n"
                                        f"Мин. темп: {round(max_temp - 273)}°C \n"
                                        f"Сегодня у нас {clouds} \n"
                                        f"Облачность: {stats['clouds']['all']}% \n"
                                        f"Скорость ветра: {wind['speed']} м/с \n"
-                                       f"Давление: {pressure} Гектопаскалей \n"
+                                       f"Давление: {prime['pressure']} Гектопаскалей \n"
                                        f"Дата: {day_time[0]} \n"
                                        f"Текущее время: {day_time[1]} \n"
                                        f"(изменения только по кнопке)")
-            self.lineEdit_town.setText('')
-        except:
-            self.label_weather.setText("Такого города не существует")
+                self.lineEdit_town.setText('')
+            except:
+                self.label_weather.setText("Такого города не существует. \n"
+                                           "Попробуйте еще раз")
 
 
 if __name__ == '__main__':
