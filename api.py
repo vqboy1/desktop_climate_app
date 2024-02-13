b454314d87f00e86d4e3444c12d57add8111f0c5
vqboy1 <152992245+vqboy1@users.noreply.github.com>
2024-02-13 14:19:20 +0300

    moe readable x vector

--- api.py
+++ api.py
@@ -70,7 +70,7 @@ def get_time_5day(data):
     date = []
 
     for i in range(len(data["list"])):
-        date.append(f'{data["list"][i]["dt_txt"][5:-6]}ч')
+        date.append(f'{data["list"][i]["dt_txt"][5:-6]}ч \n'.replace(' ', '\n'))
 
     return date
 
